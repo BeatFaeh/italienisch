ae9334120d66ca467868d07097344cb62cda8667
beat <beat@faeh.sh>
2026-08-26 09:32:13 +0200

    neu

--- partials/site-footer.php
+++ partials/site-footer.php
@@ -3,7 +3,13 @@
         <strong>Buono studio!</strong><br>
         <span>Italienisch lernen mit Wörtern und ganzen Sätzen.</span>
     </p>
+<p>
 
+    <a href="https://www.faeh.sh/italienisch/vorlagen/wortschatz.xlsx" target="_blank">
+        Wörter als Excel Liste herunterladen
+    </a>
+
+</p>
     <p>
         Design &amp; Programming by <a href="https://www.faeh.sh/" target="_blank">Beat Faeh</a>
     </p>
